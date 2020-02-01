02fbf33fc60710bc543f4be7ca658c41bfac7b3a
gradgrind <mt.bothfeld@gmail.com>
2020-02-01 12:57:38 +0100

    Added gunicorn server for "production" purposes.

--- run-zeugs.sh
+++ run-zeugs.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
-
-#thisfile=$( realpath "$0"  )
-#thisdir=$( dirname "$thisfile" )
-
-#. "$thisdir/venv/bin/activate"
-
-#cd "$(dirname "$0")"
-#. venv/bin/activate
-#cd zeugs/flask
-
-#export FLASK_APP=zeugs
-#export FLASK_ENV=development
-
-#flask run
+# This starts a gunicorn server for production purposes
+PORT=5005
 
 thisfile=$( realpath "$0"  )
 thisdir=$( dirname "$thisfile" )
-#export FLASK_APP=zeugs
-export FLASK_APP=flask_app
-export FLASK_ENV=development
+#export FLASK_ENV=development
 export ZEUGS_BASE="$thisdir"
-#cd "$thisdir/zeugs/flask"
 cd "$thisdir/zeugs"
-"$thisdir/venv/bin/python" -m flask run
+#"$thisdir/venv/bin/python" -m flask run
+"$thisdir/venv/bin/gunicorn" --workers=2 --bind=127.0.0.1:$PORT --worker-class=gthread --threads=2 "flask_app:create_app()"
